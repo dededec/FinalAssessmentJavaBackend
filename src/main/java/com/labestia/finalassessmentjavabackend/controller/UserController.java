e9145358e4da6916a69d3cba92a65cc585b551f1
dededec <daviderenacasado@gmail.com>
2022-11-17 12:57:36 +0100

    Backend delete and put.

--- src/main/java/com/labestia/finalassessmentjavabackend/controller/UserController.java
+++ src/main/java/com/labestia/finalassessmentjavabackend/controller/UserController.java
@@ -6,8 +6,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +35,27 @@ public class UserController {
     public ResponseEntity<List<User>> getAllUsers() {
         List<User> users = service.getAllUsers();
         return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @PutMapping(path = "/users")
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
+        if(user.getId() == null) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+
+        User createdUser = service.updateUser(user);
+        if(createdUser == null) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+        else {
+            return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+        }
+    }
+
+    @DeleteMapping(path = "/users/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable int id) {
+        service.deleteUser(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
--- src/main/java/com/labestia/finalassessmentjavabackend/service/UserService.java
+++ src/main/java/com/labestia/finalassessmentjavabackend/service/UserService.java
@@ -3,6 +3,7 @@ package com.labestia.finalassessmentjavabackend.service;
 import com.labestia.finalassessmentjavabackend.domain.entity.User;
 import com.labestia.finalassessmentjavabackend.repository.UserJpaRepository;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,10 +26,41 @@ public class UserService {
     }
 
     private boolean checkInput(User user) {
-        return !user.getFirstName().isEmpty()
-                && !user.getLastName().isEmpty()
-                && user.getEmail().length() > 0
+        return !isNullOrEmpty(user.getFirstName())
+                && !isNullOrEmpty(user.getLastName())
+                && !isNullOrEmpty(user.getEmail())
                 && user.getEmail().toLowerCase().matches("^([a-z])(\\w+)@([a-z]+)\\.([a-z]+)$")
-                && user.getPhoneNumber().length() > 0 && user.getPhoneNumber().matches("^(\\d+)$");
+                && !isNullOrEmpty(user.getPhoneNumber()) && user.getPhoneNumber().matches("^(\\d+)$");
+    }
+
+    public User updateUser(User updatedUser) {
+        Optional<User> userDb = repository.findById(updatedUser.getId());
+        if(userDb.isEmpty())
+        {
+            return null;
+        }
+        else {
+            User user = userDb.get();
+            if(!isNullOrEmpty(updatedUser.getFirstName()))
+                user.setFirstName(updatedUser.getFirstName());
+            if(!isNullOrEmpty(updatedUser.getLastName()))
+                user.setLastName(updatedUser.getLastName());
+            if(!isNullOrEmpty(updatedUser.getEmail()) && updatedUser.getEmail().toLowerCase().matches("^([a-z])(\\w+)@([a-z]+)\\.([a-z]+)$"))
+                user.setEmail(updatedUser.getEmail());
+            if(!isNullOrEmpty(updatedUser.getPhoneNumber()) && updatedUser.getPhoneNumber().matches("^(\\d+)$"))
+                user.setPhoneNumber(updatedUser.getPhoneNumber());
+
+            return repository.save(user);
+        }
+    }
+
+    public void deleteUser(int id) {
+        Optional<User> user = repository.findById(id);
+        if(user.isPresent())
+            repository.deleteById(id);
+    }
+
+    private boolean isNullOrEmpty(String string) {
+        return string == null || string.isEmpty();
     }
 }
