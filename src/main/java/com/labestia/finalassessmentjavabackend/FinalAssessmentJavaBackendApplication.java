28fe90bcc7aac9424ac3bee33f6a364d0d169cd4
dededec <daviderenacasado@gmail.com>
2022-11-17 10:06:32 +0100

    first commit

---
+++ src/main/java/com/labestia/finalassessmentjavabackend/FinalAssessmentJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.labestia.finalassessmentjavabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalAssessmentJavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalAssessmentJavaBackendApplication.class, args);
+	}
+
+}
